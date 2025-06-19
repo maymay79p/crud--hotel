5e04d372bf219b01f6b5ab463b75546b868b240f
maymay79p <marinacollares.bg018@academico.ifsul.edu.br>
2025-06-19 16:01:53 -0300

    Update conexao.js

--- DAW1/config/conexao.js
+++ DAW1/config/conexao.js
@@ -1,18 +1,7 @@
-const mysql = require('mysql');
+import mongoose from "mongoose";
 
-const db = mysql.createConnection({
-    host: 'localhost',        // ou o IP do servidor MySQL
-    user: 'root',             // seu usuário do MySQL
-    password: '',             // sua senha do MySQL
-    database: 'nome_do_banco' // nome do banco que você criou (exemplo: hotel_db)
-});
+const conexao = mongoose.connect("mongodb://localhost:27017/loja")
 
-db.connect((err) => {
-    if (err) {
-        console.error('Erro ao conectar ao banco de dados:', err);
-    } else {
-        console.log('Conectado ao banco de dados MySQL!');
-    }
-});
+export default conexao;
 
-module.exports = db;
+#arrumar
