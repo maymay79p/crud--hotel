76a766a1f59c477919a26e308612dcbc5fedee1d
joannelsanches <joannesanches.bg013@academico.ifsul.edu.br>
2025-06-20 17:22:18 -0300

    Update conexao.js
    sla se ta certo o npm não funciona no meu computador

--- DAW1/config/conexao.js
+++ DAW1/config/conexao.js
@@ -1,7 +1,5 @@
 import mongoose from "mongoose";
 
-const conexao = mongoose.connect("mongodb://localhost:27017/loja")
+const conexao = mongoose.connect("mongodb+srv://jlemos:@Miraculous13@cluster0.rleqqp2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 export default conexao;
-
-#arrumar
