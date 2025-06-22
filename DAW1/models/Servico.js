39af551e3f4833e5c713736783737de89e1aa9c6
maymay79p <marinacollares.bg018@academico.ifsul.edu.br>
2025-06-22 16:39:14 -0300

    Update Servico.js

--- DAW1/models/Servico.js
+++ DAW1/models/Servico.js
@@ -1,16 +1,16 @@
-import conexao from "../config/conexao.js";
+import mongoose from 'mongoose';
 
-const servicoSchema = new conexao.Schema({
+const servicoSchema = new mongoose.Schema({
     descricao: {
         type: String,
         required: true,
     },
-    mediaValor {
+    mediaValor: {
         type: Number,
         required: true,
-    }
+    },
 });
 
-const Servico = conexao.model("Servico", servicoSchema);
+const Servico = mongoose.model("Servico", servicoSchema);
 
 export default Servico;
