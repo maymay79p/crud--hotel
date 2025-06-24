cc22677c6f7317663faed83a40d16a7809a103d1
joannelsanches <joannesanches.bg013@academico.ifsul.edu.br>
2025-06-24 20:52:06 -0300

    Update conexao.js

--- DAW1/config/conexao.js
+++ DAW1/config/conexao.js
@@ -1,17 +1,5 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const uri = await mongoose.connect("mongodb://aluno:<db_password>@cluster0-shard-00-00.luoxr.mongodb.net:27017,cluster0-shard-00-01.luoxr.mongodb.net:27017,cluster0-shard-00-02.luoxr.mongodb.net:27017/?ssl=true&replicaSet=atlas-liydtq-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0");
+const conexao = await mongoose.connect("mongodb://aluno:hotel00@cluster0-shard-00-00.luoxr.mongodb.net:27017,cluster0-shard-00-01.luoxr.mongodb.net:27017,cluster0-shard-00-02.luoxr.mongodb.net:27017/?ssl=true&replicaSet=atlas-liydtq-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0")
 
-async function conectar() {
-  try {
-    await mongoose.connect(uri);
-    console.log("Conectado ao MongoDB Atlas com sucesso!");
-  } catch (error) {
-    console.error("Erro na conexão:", error);
-    process.exit(1);
-  }
-}
-
-conectar();
-
-export default mongoose;
+export default conexao; 
